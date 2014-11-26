0ab8a00097a291d7db31e4abe0f39c7d1fedf806
Richard Stones <r.stones@ucl.ac.uk>
2014-11-26 17:11:42 +0000

    Initial commit

---
+++ scripts/publications_paging.js
@@ -0,0 +1,69 @@
+var aocPagingNS = (function() {
+
+	var ns = {};
+	
+	/*
+		Converts author string from Google Scholar format to nicer looking format
+		by removing semi-colons between authors and swapping order of surnames and initals.
+	*/
+	ns.formatAuthorsString = function(authorsOrig) {
+		var authorsArray = authorsOrig.replace(/;/g, "").split(" ");
+		var result = [];
+		$.each(authorsArray, function(index, value) {
+			if (index % 2 === 0) {
+				result[index+1] = value;
+			} else {
+				result[index-1] = value;
+			}
+		});
+		return $.trim(result.join(" ")).replace(/,$/, "");
+	}
+
+	/*
+		Constructs 2D array required by DataTable from publications JSON object while also
+		concatenating required fields to display reference in standard form.
+	*/
+	ns.constructDataTableArray = function(pubsJSON) {
+		dataTableArray = []
+		$.each(pubsJSON, function() {
+			var authors = ns.formatAuthorsString(this.Authors);
+			var entry = ["<b>" +  this.Title + "</b><br>"
+								+ authors + "<br>"
+								+ (this.Link ? ("<a href=\"" + this.Link + "\">") : "") + (this.Publication ? this.Publication : "")
+								+ (this.Volume ? (", " + this.Volume) : "")
+								+ (this.Number ? (", " + this.Number) : "")
+								+ (this.Pages ? (", " + this.Pages) : "")
+								+ " (" + this.Year + ")" + (this.Link ? "</a>" : "")
+								,this.Year];
+			dataTableArray.push(entry);
+		});
+		return dataTableArray;
+	}
+
+	/*
+		Construct table to display publications data sorting by the hidden Year column
+	*/
+	ns.constructDataTable = function(data) {
+		$('#pubs-table').dataTable({
+			"aaData": data,
+			"aoColumns": [
+						{"sTitle":"Publications"},
+						{"sTitle":"Year", "bVisible": false}
+						],
+			"aaSorting": [[1,'desc']]
+		});
+	}
+
+	return ns;
+})();
+
+/*
+	Script run after page load
+*/
+$(document).ready(function() {
+	//AJAX call to retrieve publications data from server
+	// this is the file path for the live file "scripts/publications.json"
+	$.getJSON("scripts/publications.json", function(data) {
+		aocPagingNS.constructDataTable(aocPagingNS.constructDataTableArray(data));
+	});
+});
---
+++ scripts/utils.js
@@ -0,0 +1,89 @@
+insertNav = function(currentPage) {
+	//document.write(
+	return '<nav id="nav-bar"><ul>' +
+				
+					'<a href="./index.html"><li>' +
+						'Home' + 
+					'</li></a>' +
+					'<a href="./research.html"><li>' +
+						'Research' +
+					'</li></a>' +
+					'<a href="./publications.html"><li>' +
+						'Publications' +
+					'</li></a>' +
+					'<a href="./members.html"><li>' +
+						'Members' + 
+					'</li></a>' +
+					'<a href="./news.html"><li>' +
+						'News' +
+					'</li></a>' +
+					'<a href="./aboutAOC.html"><li>' +
+						'About AOC' +
+					'</li></a>' +
+				'</ul>' +
+				//'<div id="nav-filler"></div>' + 
+				'</nav>';//);
+}
+
+insertHeader = function(currentPage) {
+	document.write('<header>' +
+						'<div id="header-image"></div>' +
+						'<div id="header-title-1" class="header-title">Quantum Physics</div>' +
+						'<div id="header-title-2" class="header-title">of</div>' +
+						'<div id="header-title-3" class="header-title">Biomolecular Processes</div>' +
+						'<div id="header-title-4" class="header-subtitle">Department of Physics and Astronomy, University College London</div>' +
+					'</header>' +
+					'<div id="ucl-bar">' +
+						'<div id="central-container">' +
+							insertNav(currentPage) +
+							'<img src="./media/ucl_logo_small-3.png" id="ucl-logo-small">' +
+						'</div>' +
+					'</div>');
+}
+
+/*
+Inserts footer into html document including copyright statement dated from 2013
+*/
+insertFooter = function() {
+	var d = new Date();
+	year = d.getFullYear();
+	document.write('<footer>Copyright &copy 2013 ' + (year > 2013 ? '- ' + year : '') + 	
+									' Alexandra Olaya-Castro | Designed by Richard Stones </footer>');
+}
+
+/*
+	Allow nav bar to follow page down whilst scrolling
+*/
+/*
+window.onload = function() {
+
+  function getScrollTop() {
+    if (typeof window.pageYOffset !== 'undefined' ) {
+      // Most browsers
+      return window.pageYOffset;
+    }
+
+    var d = document.documentElement;
+    if (d.clientHeight) {
+      // IE in standards mode
+      return d.scrollTop;
+    }
+
+    // IE in quirks mode
+    return document.body.scrollTop;
+  }
+
+  window.onscroll = function() {
+    var box = document.getElementById('nav-bar'),
+        scroll = getScrollTop();
+
+    if (scroll <= 180) {
+      box.style.top = "180px";
+    }
+    else {
+      box.style.top = (scroll) + "px";
+    }
+  };
+
+};
+*/
